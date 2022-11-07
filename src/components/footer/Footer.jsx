0b4cafbad47770d060fc746eff73301d4a43def4
Thomas Hart <thomas.hart@uwaterloo.ca>
2022-11-07 14:08:56 -0500

    Added devpost link in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './footer.css'
-import {BsLinkedin, BsGithub} from 'react-icons/bs'
+import { BsLinkedin, BsGithub } from 'react-icons/bs'
+import { SiDevpost } from 'react-icons/si'
 
 const Footer = () => {
   return (
     <footer>
       <a href='#' className='footer__logo'>Thomas Hart</a>
       <div className='footer__socials'>
-        <a href='https://linkedin.com/in/thomashart17'><BsLinkedin/></a>
-        <a href='https://github.com/thomashart17'><BsGithub/></a>
+        <a href='https://linkedin.com/in/thomashart17'><BsLinkedin /></a>
+        <a href='https://github.com/thomashart17'><BsGithub /></a>
+        <a href='https://devpost.com/thomashart17'><SiDevpost /></a>
       </div>
       <div className="footer__copyright">
         <small>&copy; Thomas Hart</small>
