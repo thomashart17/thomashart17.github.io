97802107b22c0982c47efab6d1f0011db4feffeb
Thomas Hart <thomas.hart@uwaterloo.ca>
2023-09-19 02:08:12 -0400

    update about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -54,13 +54,19 @@ const About = () => {
           <div className='about__me-image'>
             <img src={Me} alt='' />
           </div>
-        </div>
-        <div className='about__content'>
           <div className='about__paragraph'>
             <p>
-              I'm a second year computer engineering student at the University of Waterloo interested in both computer software and hardware.
+              I'm a 2B computer engineering student at the University of Waterloo, with a keen interest in both computer software and hardware. 
+              I have completed 3 co-op terms so far, working in a variety of roles in the diverse fields of software development. 
+              This experience has allowed me to gain experience in a variety of languages, frameworks, and tools, and has allowed me to develop the ability to quickly learn new technologies and adapt to new roles. 
+              Outside of my academic and professional endeavors, I've dedicated my time to a variety of personal projects, which have allowed me to explore my interests in more depth and further expand my skillset. 
+              In addition to the projects I have done on my own time, I have built projects for submission at 8 hackathons, winning prizes at 2 of them.
+              I am always open to hearing about exciting new opportunities and collaborating on new projects. 
+              If you're interested in connecting or collaborating, please feel free to reach out to me!
             </p>
           </div>
+        </div>
+        <div className='about__content'>
           <div className='about__skills'>
             <h2>Skills</h2>
             <h3>Languages</h3>
