080c8e9c2023059acf5fadff4b736c939dc9a9f7
Thomas Hart <thomas.hart@uwaterloo.ca>
2025-02-20 22:37:25 -0500

    fix resume name issue

--- src/components/header/HeaderButtons.jsx
+++ src/components/header/HeaderButtons.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Resume from '../../assets/ThomasHartResume.pdf'
 
 const HeaderButtons = () => {
   return (
     <div className='header__buttons'>
-      <a href={Resume} download className='btn'>R&#233;sum&#233;</a>
+      <a href='ThomasHartResume.pdf' download className='btn'>R&#233;sum&#233;</a>
       <a href='#contact' className='btn btn-primary'>Contact</a>
     </div>
   )
